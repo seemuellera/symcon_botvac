8c86be98064771d97ec4005988e81940a53d3523
seemuellera <andreas.seemueller@gmail.com>
2017-04-30 00:15:30 +0200

    debug

--- SymconBotvac/module.php
+++ SymconBotvac/module.php
@@ -140,7 +140,7 @@
 		$form['actions'][] = Array("type" => "Button", "label" => "Resume Cleaning", "onClick" => 'BOTVAC_ResumeCleaning($id);');
 		$form['actions'][] = Array("type" => "Button", "label" => "Send to Base", "onClick" => 'BOTVAC_SendToBase($id);');
 
-		print_r($form['actions']);
+		print_r(json_encode($form));
 
 
 		// Return the completed form
